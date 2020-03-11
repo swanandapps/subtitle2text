6c802403d919b6f5eba7d7dfae2823b2c29dd24d
SWANAND KADAM <jonny@SWANANDs-MacBook-Pro.local>
2020-03-11 08:26:33 +0530

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,200 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+   
+   <div id="compiler">
+   <div id="textarea">
+    <textarea v-model="subtitle" type="text" name="subtitle" id="subtitlearea"></textarea>
+
+    <button id="subm" @click="RUN()">RUN</button>
+   
+   </div>
+
+    <div id="output">
+
+<div id="bharatDIV">
+        
+{{this.output}}
+
+</div>
+
+
+    </div>
+</div>
+   
+
+    </div>
+  
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+
+data() {
+  return {
+    
+subtitle:this.subtitle,
+output:''
+
+  }
+},
   props: {
     msg: String
   }
+
+,
+methods: {
+  
+RUN: function()
+{
+
+var subtitle=this.subtitle
+  
+
+console.log(subtitle.length)
+
+
+
+
+for(var i=0;i<subtitle.length;i++)
+{
+
+if(subtitle[i]=='[')
+{
+
+
+  while(subtitle[i]!=']')
+  {
+
+i++;
+
+
+  }
+
+
 }
+
+else
+{
+
+
+this.output=this.output+subtitle[i]
+
+}
+
+
+}
+
+
+
+}
+
+}
+}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+#bharatP{
+
+
+text-align: left;
+  display: table-cell;
+    font-family: monospace
+}
+#versionNumber{
+
+
+text-align: left;
+  display: table-cell;
+    font-family: monospace;
+    padding-left: 6%
+}
+
+#bharatDIV{
+
+    padding-top: 2%;
+    padding-left: 2%;
+    overflow: auto;
+max-height: 100vh;
+background-color: black;
+padding-bottom: 5%
+
+}
+
+#linebylineOutput
+{
+
+
+
+height: 10px;
+    text-align: left;
+    
+
+
+}
+#errorstack{
+
+text-align: left;
+padding-left: 2.5%;
+padding-top: 6%;
+font-weight: bold;
+color:rgb(231, 83, 83);
+
+}
+
+#compiler{
+
+  display: flex;
+}
+
+#textarea{
+
+  width: 50%
+}
+#output{
+
+  height: 400px;
+  width: 50%;
+  
+    color: white;
+  
+}
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+.hello {
+  display: grid;
+}
+
+#subtitlearea {
+  width: 90%;
+  height: 400px;
+  background-color: whitesmoke;
+    font-family: monospace;
+    font-size: medium;
+    border: none
+}
+
+#subm {
+  width: 10%;
+  height: 50px;
 }
 </style>
